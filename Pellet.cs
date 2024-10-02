69c69c59052d7ef90566a1cbdf910d8e84efc0c3
ScrubbyAlien <samuel.eskilson@gmail.com>
2024-10-02 14:26:26 +0200

    changed pellet bounding box

--- Pellet.cs
+++ Pellet.cs
@@ -6,6 +6,19 @@ public class Pellet : Entity
 {
     public Pellet() : base("pacman") { }
 
+    public override FloatRect Bounds
+    {
+        get
+        {
+            FloatRect bounds = base.Bounds;
+            bounds.Left += 6;
+            bounds.Width -= 12;
+            bounds.Top += 6;
+            bounds.Height -= 12;
+            return bounds;
+        }  
+    }
+
     public override void Create(Scene scene)
     {
         base.Create(scene);
